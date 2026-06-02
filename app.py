462a863da9b0a7330eb7f41ad497d005a73c85ed
Bernardo Giancristofaro Carvalho <giancristofarobernardo@gmail.com>
2026-06-02 18:48:27 -0300

    Substituindo mostrador de tempo por cronómetro de quanto tempo juntos

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 import streamlit as st 
 from datetime import datetime
 
+
+def mostrar_tempo_juntos(data_inicio):
+    agora = datetime.now()
+    tempo = agora - data_inicio
+
+    dias = tempo.days
+    horas = tempo.seconds // 3600
+    minutos = (tempo.seconds % 3600) // 60
+    segundos = tempo.seconds % 60
+
+    st.header("Estamos juntos há  ❤️")
+
+
+    st.markdown(f"""
+    # ⏳ {dias} dias
+    ## {horas} horas
+    ## {minutos} minutos
+    ## {segundos} segundos
+    """)
+
+
 st.set_page_config(
     page_title="Nossa História ❤️",
     page_icon="❤️",
@@ -15,9 +36,11 @@ with co12:
     inicio = datetime(2023, 11, 25)
 
     agora = datetime.now()
+
     tempo = agora - inicio 
 
     st.header('Estamos juntos há:')
+    
     st.write(f'{tempo.days} dias')
 
     st.image('foto_juntos.jpg', width=700)
